415ba25148b2886647d2af3a2a2c5c734c858808
Rafael <rafael.silva@luizalabs.com>
2022-06-26 15:56:15 -0300

    conn, send and receive events

--- src/containers/steps/index.tsx
+++ src/containers/steps/index.tsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import socket from "../../io/io";
 
 const StepsIO: React.FC = () => {
+    const roomName = "ReceiptDetails";
+    const [id, setId] = useState("");
+    const [confirmation, setConfirmation] = useState(false);
+
+    useEffect(() => {
+        socket.emit("join_room", roomName);
+    }, [id]);
+
+    socket.on("connect", () => {
+        setId(socket.id);
+    });
+
+    useEffect(() => {
+        socket.on("received_confirmation", (data) => {
+            setConfirmation(data);
+        });
+    }, [socket]);
+
+    const sendEvent = () => {
+        socket.emit("step_confirmation", {
+            roomName,
+            confirmation: true
+        });
+    }
+
+    const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+        setConfirmation(true);
+        sendEvent();
+    }
 
     return <>
-        HELLO WORLD
+        <button onClick={handleClick}>CONFIRMAR</button>
+        <div>
+            ID: {id}
+            <br />
+            Is Confirmed:
+            {confirmation ? "YES" : "NO"}
+        </div>
     </>
 
 }
